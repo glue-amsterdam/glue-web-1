fe781d6de0d10b7e0e0a27b23bb7ae4ee37e916d
Javier Azua <azua.javi@gmail.com>
2024-09-15 19:43:19 -0300

    A little refactory of classes and page.tsx

--- src/app/components/background/logo-main.tsx
+++ src/app/components/background/logo-main.tsx
@@ -8,22 +8,22 @@ function LogoMain({}) {
         <img
           src="logos/GLUE_G.svg"
           alt="G"
-          className="size-32 p-4 md:size-38 lg:size-44 xl:size-50 absolute top-0 left-0 bg-black"
+          className="mainLogoLetter top-0 left-0"
         />
         <img
           src="logos/GLUE_L.svg"
           alt="L"
-          className="size-32 p-4 md:size-38 lg:size-44 xl:size-50 absolute top-0 right-0 bg-black"
+          className="mainLogoLetter top-0 right-0"
         />
         <img
           src="logos/GLUE_U.svg"
           alt="U"
-          className="size-32 p-4 md:size-38 lg:size-44 xl:size-50 absolute bottom-0 left-0 bg-black"
+          className="mainLogoLetter bottom-0 left-0"
         />
         <img
           src="logos/GLUE_E.svg"
           alt="E"
-          className="size-32 p-4 md:size-38 lg:size-44 xl:size-50 absolute bottom-0 right-0 bg-black"
+          className="mainLogoLetter bottom-0 right-0"
         />
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,16 @@ import LogoMain from "./components/background/logo-main";
 export default function Home() {
   return (
     <main className="fixed inset-0 flex justify-center items-center">
+      <BackGround />
+    </main>
+  );
+}
+
+function BackGround() {
+  return (
+    <>
       <LogoMain />
       <BackgroundGrid />
-    </main>
+    </>
   );
 }
