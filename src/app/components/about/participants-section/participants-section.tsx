b426453b5c46b885a5967ad756c8a00ddb3be51b
Javier Azua <azua.javi@gmail.com>
2024-10-11 04:10:46 -0300

    participant-section-animations

--- src/app/components/about/participants-section/participants-section.tsx
+++ src/app/components/about/participants-section/participants-section.tsx
@@ -26,62 +26,69 @@ export default function ParticipantsSection({
 
   return (
     <section
-      className="mb-12 container mx-auto px-4"
+      className="mb-12 space-y-10 container mx-auto px-4"
       aria-labelledby="participants-heading"
     >
       <motion.h2
         id="participants-heading"
-        className="text-4xl text-center uppercase font-bold"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        className="text-7xl uppercase tracking-wider font-bold"
+        initial={{ opacity: 0, x: 30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
         transition={{ delay: 1 }}
       >
         Participants
       </motion.h2>
-      <Carousel
-        className="w-[70%] mx-auto sm:w-full"
-        plugins={[
-          Autoplay({
-            stopOnMouseEnter: true,
-            delay: 2000,
-          }),
-        ]}
+      <motion.div
+        initial={{ rotate: 10 }}
+        whileInView={{ rotate: 0 }}
+        viewport={{ amount: "all", once: true, margin: "40%" }}
       >
-        <CarouselContent>
-          {participants.map((participant, index) => (
-            <CarouselItem
-              key={index}
-              className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-            >
-              <Link
-                target="_blank"
-                href={`/member/${encodeURIComponent(participant.name)}`}
+        <Carousel
+          className="w-[70%] mx-auto sm:w-full"
+          plugins={[
+            Autoplay({
+              stopOnMouseEnter: true,
+              delay: 2000,
+            }),
+          ]}
+        >
+          <CarouselContent className="rounded-none">
+            {participants.map((participant, index) => (
+              <CarouselItem
+                key={index}
+                className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
               >
-                <div className="p-1">
-                  <Card className="border-none shadow-sm hover:shadow-md transition-shadow duration-300">
-                    <CardContent className="flex aspect-square items-center justify-center p-0">
-                      <div className="relative w-full h-full group">
-                        <img
-                          src={participant.image}
-                          alt={participant.name}
-                          className="w-full h-full object-cover rounded-lg"
-                        />
-                        <div className="absolute inset-0 bg-black bg-opacity-50 text-white flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity rounded-lg">
-                          <p className="text-center font-semibold text-sm">
-                            {participant.name}
-                          </p>
+                <Link
+                  target="_blank"
+                  href={`/member/${encodeURIComponent(participant.name)}`}
+                >
+                  <div className="p-1">
+                    <Card className="border-none shadow-sm hover:shadow-md transition-shadow duration-300">
+                      <CardContent className="flex aspect-square items-center justify-center p-0">
+                        <div className="relative w-full h-full group">
+                          <img
+                            src={participant.image}
+                            alt={participant.name}
+                            className="w-full h-full object-cover"
+                          />
+                          <div className="absolute inset-0 bg-black bg-opacity-50 text-white flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity rounded-lg">
+                            <p className="text-center font-semibold text-sm">
+                              {participant.name}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </div>
-              </Link>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="hidden md:flex text-black" />
-        <CarouselNext className="hidden md:flex text-black" />
-      </Carousel>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </Link>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden md:flex text-black" />
+          <CarouselNext className="hidden md:flex text-black" />
+        </Carousel>
+      </motion.div>
     </section>
   );
 }
