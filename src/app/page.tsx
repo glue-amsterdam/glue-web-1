0daffad10349a7a6e5dfaa80c2de36d8176d41bb
Javier Azua <azua.javi@gmail.com>
2024-10-14 04:12:35 -0300

    Trying to build 1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import { Suspense } from "react";
 import BackgroundGrid from "./components/background-grid";
 import LogoMain from "./components/home-page/logo-main";
 import ClickAreas from "./components/home-page/main-buttons/click-areas";
 
 export default function Home() {
   return (
-    <main className="fixed inset-0">
-      <ClickAreas />
-      <BackGround />
-    </main>
+    <Suspense>
+      <main className="fixed inset-0">
+        <ClickAreas />
+        <BackGround />
+      </main>
+    </Suspense>
   );
 }
 
