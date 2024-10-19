fc77424bd7ac5a0f3cbbcd09a38ded60d2e4dd63
Javier Azua <azua.javi@gmail.com>
2024-10-19 18:29:56 -0300

    remove about for data fetching 2.0

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,4 @@
 import { Metadata } from "next";
-import { Suspense } from "react";
-import Background from "@/app/components/background";
-import AboutPageContainer from "@/app/about/about-page-container";
 
 export const metadata: Metadata = {
   title: "GLUE - About",
