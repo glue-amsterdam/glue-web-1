456214c90d94d0db3f4277519bf9f7fe75b230bc
Javier Azua <azua.javi@gmail.com>
2024-11-22 02:17:53 -0300

    Changed the way the slug of participant is handled

--- src/app/participants/[slug]/page.tsx
+++ src/app/participants/[slug]/page.tsx
@@ -2,14 +2,16 @@ import { Suspense } from "react";
 import { fetchParticipant, fetchMapById } from "@/utils/api";
 import CenteredLoader from "@/app/components/centered-loader";
 import ParticipantClientPage from "@/app/participants/[slug]/participants-client-page";
+import { ParticipantUser } from "@/schemas/usersSchemas";
 
-export default async function ParticipantPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+interface PageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function ParticipantPage({ params }: PageProps) {
   const { slug } = await params;
-  const participant = await fetchParticipant(slug);
+  const participant: ParticipantUser = await fetchParticipant(slug);
 
   let mapData = null;
   if (participant && "mapId" in participant) {
