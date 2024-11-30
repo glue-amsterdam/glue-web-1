2321f4e3d119c6873357f9ec1a6d00b8f13ae8d2
Javier Azua <azua.javi@gmail.com>
2024-11-29 23:39:09 -0300

    Remove unused about section components and forms to streamline the admin dashboard and eliminate unnecessary code bloat

--- src/app/admin/sections/about-carousel-section.tsx
+++
@@ -1,8 +0,0 @@
-import AboutCarouselSectionForm from "@/app/admin/forms/about-carousel-form";
-import { fetchAboutCarousel } from "@/utils/api/admin-api-calls";
-
-export default async function AboutCarouselSection() {
-  const mainColors = await fetchAboutCarousel();
-
-  return <AboutCarouselSectionForm initialData={mainColors} />;
-}
