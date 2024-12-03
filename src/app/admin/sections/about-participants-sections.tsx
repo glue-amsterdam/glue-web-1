facd3ef3857b5a9c22e142976eb928c08b69663b
Javier Azua <azua.javi@gmail.com>
2024-12-03 16:53:49 -0300

    feat: update ParticipantsSectionForm to reset on initialData change and improve error handling in AboutParticipantsSection component

--- src/app/admin/sections/about-participants-sections.tsx
+++ src/app/admin/sections/about-participants-sections.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import useSWR from "swr";
 import ParticipantsSectionForm from "@/app/admin/forms/about-participants-form";
-import { fetchAboutParticipants } from "@/utils/api/admin-api-calls";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import { ParticipantsSectionHeader } from "@/schemas/participantsAdminSchema";
 
-export default async function AboutParticipantsSection() {
-  const carouselData = await fetchAboutParticipants();
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  return <ParticipantsSectionForm initialData={carouselData} />;
+export default function AboutParticipantsSection() {
+  const {
+    data: particpantsHeader,
+    error,
+    isLoading,
+  } = useSWR<ParticipantsSectionHeader>(
+    "/api/admin/about/participants",
+    fetcher
+  );
+
+  if (isLoading) return <LoadingSpinner />;
+  if (error) return <div>Failed to load participants data</div>;
+  if (!particpantsHeader) return <div>No participants data available</div>;
+
+  return <ParticipantsSectionForm initialData={particpantsHeader} />;
 }
