567adf85c1e500008220906cad12a5e3853d30f9
Javier Azua <azua.javi@gmail.com>
2024-12-04 13:17:54 -0300

    feat: Add SponsorModal component for editing sponsor details with a dialog interface in the admin panel

---
+++ src/app/admin/components/sponsors-modal.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Sponsor, SponsorType } from "@/schemas/sponsorsSchema";
+import SponsorForm from "@/app/admin/forms/about-sponsors-form";
+import { DialogDescription } from "@radix-ui/react-dialog";
+
+interface SponsorModalProps {
+  sponsor: Sponsor;
+  sponsorTypes: SponsorType[];
+  onClose: () => void;
+  onSponsorUpdated: () => void;
+}
+
+export default function SponsorModal({
+  sponsor,
+  sponsorTypes,
+  onClose,
+  onSponsorUpdated,
+}: SponsorModalProps) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    onClose();
+  };
+
+  const handleSponsorUpdated = () => {
+    onSponsorUpdated();
+    handleClose();
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="w-[90%] md:w-[70%] lg:w-[80%] text-black max-h-[90%] overflow-y-scroll overflow-x-hidden">
+        <DialogDescription className="sr-only">
+          Edit modal for {sponsor.name}
+        </DialogDescription>
+        <DialogHeader>
+          <DialogTitle>Edit: {sponsor.name}</DialogTitle>
+        </DialogHeader>
+        <SponsorForm
+          initialData={sponsor}
+          sponsorTypes={sponsorTypes}
+          onSponsorUpdated={handleSponsorUpdated}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
