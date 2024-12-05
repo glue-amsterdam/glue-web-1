41834e769fd252273c673f5b0a9356c8cc9d7496
Javier Azua <azua.javi@gmail.com>
2024-12-04 21:41:18 -0300

    Added plans section in admin dashboard

---
+++ src/app/admin/sections/plans-two-plan-sections.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import PlanPayedForm from "@/app/admin/forms/plans-payed-forms";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+import { usePlan } from "@/app/hooks/usePlan";
+
+export default function PlanTwoSection() {
+  const { plan, isLoading, isError, mutate } = usePlan("planId-2");
+
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return <div>Error loading plan</div>;
+  if (!plan) return <div>Plan not found</div>;
+
+  return <PlanPayedForm plan={plan} mutate={mutate} />;
+}
