2bd8966bb9264b561590ce747296bf9bea0c3015
Javier Azua <azua.javi@gmail.com>
2025-01-18 00:40:39 -0300

    Enhance ParticipantsPage layout with flexbox for better responsiveness and add 'about-w' class to ParticipantsListSkeleton for improved styling

--- src/app/components/participants/participants-list-skeleton.tsx
+++ src/app/components/participants/participants-list-skeleton.tsx
@@ -1,6 +1,6 @@
 export default function ParticipantsListSkeleton() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 about-w mx-auto">
       {[...Array(6)].map((_, index) => (
         <div
           key={index}
