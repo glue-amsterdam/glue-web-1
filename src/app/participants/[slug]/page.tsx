2c282775b82e7cd297b0f24b789aed5524393c4d
Javier Azua <azua.javi@gmail.com>
2025-03-12 06:54:46 -0300

    Enhance participant metadata generation: add dynamic metadata for participant pages including title, description, and Open Graph data

--- src/app/participants/[slug]/page.tsx
+++ src/app/participants/[slug]/page.tsx
@@ -4,9 +4,62 @@ import ParticipantClientPage from "@/app/participants/[slug]/participants-client
 import { ParticipantClientResponse } from "@/types/api-visible-user";
 import PendingParticipant from "@/app/participants/[slug]/pending-participant";
 import DeclinedParticipant from "@/app/participants/[slug]/declined-participant";
+import { config } from "@/env";
 
 interface PageProps {
   params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata({ params }: PageProps) {
+  const { slug } = await params;
+  const participant: ParticipantClientResponse = await fetchParticipant(slug);
+
+  // Default image (fallback)
+  const defaultImage = `${config.baseUrl}/${config.cityName}/og-image.jpg`;
+
+  // Select the first image if available
+  const imageUrl =
+    participant.images.length > 0
+      ? participant.images[0].image_url
+      : defaultImage;
+
+  return {
+    title: `GLUE ${config.cityName} - ${
+      participant.user_info.user_name ?? "Unknown"
+    }`,
+    description:
+      participant.short_description ??
+      `View details about ${participant.user_info.user_name}. A GLUE participant.`,
+    keywords: ["GLUE", "participant", participant.user_info.user_name],
+    openGraph: {
+      title: `GLUE ${config.cityName} - ${
+        participant.user_info.user_name ?? "Unknown"
+      }`,
+      description:
+        participant.short_description ??
+        `View details about ${participant.user_info.user_name}.A GLUE participant.`,
+      url: `/participants/${slug}`,
+      type: "profile",
+      images: [
+        {
+          url: imageUrl,
+          width: 1200,
+          height: 630,
+          alt: `Profile image of ${participant.user_info.user_name}`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `GLUE ${config.cityName} - ${
+        participant.user_info.user_name ?? "Unknown"
+      }`,
+      description:
+        participant.short_description ??
+        `View details about ${participant.user_info.user_name}.`,
+      images: [imageUrl],
+    },
+  };
 }
 
 export default async function ParticipantPage({
