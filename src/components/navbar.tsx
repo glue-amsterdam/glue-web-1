656f582e8f142e21a937d3d8ec2b84e82594ece8
azuaj <azua.javi@gmail.com>
2026-06-03 21:59:57 -0300

    feat: add responsive navigation bar component
    - Introduced a new NavBar component with links and buttons for navigation.
    - Implemented responsive design for mobile and desktop views.
    - Integrated dynamic highlighting of active links based on the current pathname.
    - Included a conditional ExhibitorsNavbar that displays when on the exhibitors page.

---
+++ src/components/navbar.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Suspense, useRef, type ReactNode } from "react";
+
+import { cn } from "@/lib/utils";
+
+import LogoWithLink from "./LogoWithLink";
+import BigButton from "./big-button";
+import ExhibitorsNavbar from "./navbar/exhibitors-navbar";
+import MainContainer from "./main-container";
+
+const navItems = {
+  buttons: [
+    {
+      label: "Participate",
+      href: "/participate",
+    },
+    {
+      label: "Visit",
+      href: "/visit",
+    },
+  ],
+  links: [
+    {
+      label: "Exhibitors",
+      href: "/exhibitors",
+    },
+    {
+      label: "Map",
+      href: "/map",
+    },
+    {
+      label: "Program",
+      href: "/program",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+};
+
+const Block = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => (
+  <div
+    className={cn(
+      "border-b lg:border-b-2 border-(--black-color) py-[12px] flex items-center justify-between bg-(--background-color)",
+      className
+    )}
+  >
+    {children}
+  </div>
+);
+
+const Container = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return <div className={cn("flex items-center", className)}>{children}</div>;
+};
+
+const Buttons = () => {
+  return (
+    <Container className="sm:gap-[30px] gap-[15px]">
+      {navItems.buttons.map((item, i) => (
+        <BigButton
+          as="link"
+          key={i}
+          label={item.label}
+          href={item.href}
+          mode="navbar"
+          fontSize="base"
+        />
+      ))}
+    </Container>
+  );
+};
+
+const Links = ({ className }: { className?: string }) => {
+  const pathname = usePathname();
+
+  return (
+    <Container className={cn("sm:gap-[30px] gap-[20px]", className)}>
+      {navItems.links.map((item, i) => (
+        <Link
+          key={i}
+          href={item.href}
+          className={cn(
+            "transition-colors duration-100 text-[15px] leading-[15px] lg:text-[23px] lg:leading-[29px]",
+            pathname === item.href
+              ? "text-[var(--primary-color)]"
+              : "text-[var(--black-color)]"
+          )}
+        >
+          {item.label}
+        </Link>
+      ))}
+    </Container>
+  );
+};
+
+const NavBar = () => {
+  const pathname = usePathname();
+  const showExhibitorsNav = pathname === "/exhibitors";
+  return (
+    <div className="fixed font-normal top-0 w-full z-50">
+      <nav className="bg-(--white-color)">
+        <MainContainer>
+          <Block className="h-(--nav-primary-h-mobile) lg:h-(--nav-primary-h)">
+            <div className="lg:w-[250px]">
+              <LogoWithLink className="size-10 lg:size-[60px] lg:hover:scale-105 lg:transition-all lg:duration-100" />
+            </div>
+            <Links className="hidden lg:flex" />
+            <Buttons />
+          </Block>
+          <Block className="flex lg:hidden h-(--nav-secondary-h-mobile)">
+            <Links />
+          </Block>
+        </MainContainer>
+      </nav>
+      {showExhibitorsNav && (
+        <div>
+          <MainContainer>
+            <Suspense fallback={null}>
+              <ExhibitorsNavbar />
+            </Suspense>
+          </MainContainer>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
